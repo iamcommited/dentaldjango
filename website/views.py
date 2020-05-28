06fe1fb31d3200878ad6193c2ed8dececf0e1c65
Taiwo Hassan <taiwoogunfuyii27@gmail.com>
2020-05-28 16:00:30 +0100

    making the website general for any client, blog not working yet

--- website/views.py
+++ website/views.py
@@ -4,6 +4,15 @@ from django.core.mail import send_mail
 def home(request):
 	return render(request, 'home.html', {})
 
+def about(request):
+	return render(request, 'about.html', {})
+
+def service(request):
+	return render(request, 'service.html', {})
+
+def pricing(request):
+	return render(request, 'pricing.html', {})
+
 def contact(request):
 	if request.method == "POST":
 
